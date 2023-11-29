223ac027e95dd1b331ef976834b0e0261119887f
Jeff-Moreau <119882736+Jeff-Moreau@users.noreply.github.com>
2023-11-29 08:22:31 -0500

    This is Jeff just left some comments in your two scripts.. also you should push code reviews so the group can check out what you have done.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    // looks like some of your menu options are a little off
+    // options are overlapping on my end, Save and Quit and Resume Game is line wrapping oddly.
+    // again just some thoughts.
+    // also was thinking maybe some different text font would be nice.
+
     [SerializeField] private GameObject PausePanel;
 
     // Update is called once per frame
